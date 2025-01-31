a4f3e70683a7ec4996d21934fc88ecb9ae08ed2c
Seth Chandler <sethbc@sethbc.org>
2025-01-30 19:50:45 -0600

    Create macos.sh
    add macos.sh

---
+++ macos.sh
@@ -0,0 +1,7 @@
+
+
+defaults -currentHost write -globalDomain NSStatusItemSpacing -int 8
+defaults -currentHost write -globalDomain NSStatusItemSelectionPadding -int 8
+
+#defaults -currentHost delete -globalDomain NSStatusItemSpacing
+#defaults -currentHost delete -globalDomain NSStatusItemSelectionPadding
